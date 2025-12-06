562ab32a5ec2d866ffb18446ea543c42506800ee
gufranthakur <gufranabdulrawoofthakur@gmail.com>
2025-12-06 15:50:10 +0530

    hello

--- core/src/main/java/com/ideaspace/IdeaSpace.java
+++ core/src/main/java/com/ideaspace/IdeaSpace.java
@@ -14,6 +14,7 @@ import com.kotcrab.vis.ui.VisUI;
 public class IdeaSpace extends ApplicationAdapter {
 
     private LectureHandler lectureHandler;
+    //helloooooo
 
     public HomeScreen homeScreen;
     public Space space;
