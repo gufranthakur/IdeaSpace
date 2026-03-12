6bdf92e8290e9b9387d94dcb6a171d461221ef2d
Gufran Thakur <gufranabdulrawoofthakur@gmail.com>
2026-03-12 11:21:54 +0530

    Gufran - Removed redundant model

--- core/src/main/java/com/ideaspace/handlers/ModelHandler.java
+++ core/src/main/java/com/ideaspace/handlers/ModelHandler.java
@@ -67,8 +67,6 @@ public class ModelHandler {
         addModelToLibrary("Mechanical-Keyboard", "models/misc/mechanicalkeyboard_split.glb");
         addModelToLibrary("Duck", "models/misc/rubber_duck.glb");
 
-        loadModel(modelLibrary.get("Duck"));
-        getModelInstance("Duck").transform.idt().scale(0.0075f, 0.0075f, 0.0075f);
     }
 
     private void addModelToLibrary(String name, String path) {
